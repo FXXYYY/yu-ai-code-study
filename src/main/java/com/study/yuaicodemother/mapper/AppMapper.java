aadf7e9a5e63d7a5affa98243398ae8f245bad19
Astra <8676993+astrafxy@user.noreply.gitee.com>
2026-04-22 21:38:42 +0800

    【ai 应用生成】 1. Langchain4j 配置 2. 大模型基本调用 3. 结构化输出 4. 流式输出 5. 策略 + 门面 + 执行器 + 混合模式实现从代码生成->解析->执行 6. 应用部署
    优化点：提示次配置化

---
+++ src/main/java/com/study/yuaicodemother/mapper/AppMapper.java
@@ -0,0 +1,14 @@
+package com.study.yuaicodemother.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.study.yuaicodemother.module.entity.App;
+
+/**
+ * 应用 映射层。
+ *
+ * @author fxy
+ * @since 2026-04-21
+ */
+public interface AppMapper extends BaseMapper<App> {
+
+}
---
+++ src/main/java/com/study/yuaicodemother/service/AppService.java
@@ -0,0 +1,31 @@
+package com.study.yuaicodemother.service;
+
+import com.mybatisflex.core.query.QueryWrapper;
+import com.mybatisflex.core.service.IService;
+import com.study.yuaicodemother.module.dto.app.AppQueryRequest;
+import com.study.yuaicodemother.module.entity.App;
+import com.study.yuaicodemother.module.entity.User;
+import com.study.yuaicodemother.module.vo.AppVO;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+/**
+ * 应用 服务层。
+ *
+ * @author fxy
+ * @since 2026-04-21
+ */
+public interface AppService extends IService<App> {
+
+    AppVO getAppVO(App app);
+
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
+
+    List<AppVO> getAppVOList(List<App> appList);
+
+    Flux<String> chatToGenCode(Long appId, String message, User user);
+
+    String deployApp(Long appId, User loginUser);
+
+}
